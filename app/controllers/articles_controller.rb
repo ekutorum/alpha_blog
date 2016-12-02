3ed12e0637e7155859a06bb094349fe691b44886
Hector M <ekutorum@gmail.com>
2016-12-02 17:16:44 +0000

    Fix create and show, add flash notice

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,22 +1,26 @@
 class ArticlesController < ApplicationController
 
-def new
-  @article = Article.new
-end
+  def new
+    @article = Article.new
+  end
 
-def create
-  # render plain: params[:article].inspect
-  @article = Article.new(article_params)
-  @article.save
-  redirect_to articles_show(@article)
-end
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:notice] = "Article was succesfully created."
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
+  end
+  
+  def show
+    @article = Article.find(params[:id])
+  end
 
-private
-
-def article_params
- params.require(:article).permit(:title, :description)
-end
-
-
+  private
+    def article_params
+      params.require(:article).permit(:title, :description)
+    end
 
 end
